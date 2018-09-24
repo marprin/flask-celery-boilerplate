30ed922f548495eefe9ee4048c2af8d16a3c7f6d
marprin <marprin.hennes@acommerce.asia>
2018-09-24 18:20:30 +0700

    Change load config from object rather than file

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
 from apps.auth.views import auth
+from settings import settings
 
 
 def create_app(config=None):
     app = Flask(__name__, instance_relative_config=True)
 
-    if config is None:
-        app.config.from_pyfile('settings.py', silent=True)
-    else:
-        app.config.from_mapping(config)
+    app.config.from_object(settings)
 
     # Register blueprint here
     app.register_blueprint(auth)
